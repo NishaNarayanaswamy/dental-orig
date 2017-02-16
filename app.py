b43e3538388bda2eff823b810baceedebffce122
NishaNarayanaswamy <nisha.narayanaswamy@sikkasoftware.com>
2017-02-16 10:27:45 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import json
 import os
 import urllib2
 from urllib2 import urlopen
+import time
+import datetime
+import re
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -27,12 +30,24 @@ def webhook():
 
 def makeWebhookResult(req):
 	
+	# demo account login
 	login_url= 'https://api.sikkasoft.com/auth/v2/provider_accounts?un=ddemo2&pw=$Sikka4040&app_id=cf345eef7cb42a39cff6972d57fe6149&app_key=8003044355153fe930ec82ca091e334e&encrypted=false&device_id=fOinX6aGKzc:APA91bHer7V6YQM5re379jNRlc4fnXBmo3ElTB0ivPgINJ76HURuabkzKXmXFZ2ITk9yBFHHDu3dCbq2s8RcuDhWdqyC8BYSAdajjp8ep3TaC_T8k4dHZJZJ-cDeH0NU6ZRjOY1O6ljb&device_type=android'
 	html = urlopen(login_url)
 	login_response = json.load(html)
 	request_key = login_response['profiles'][0]['request_key']
 	domain = login_response['profiles'][0]['profile_type']
-	speech = "hello Sikka " + request_key + " domain " + domain
+	
+	speech = ""
+	
+	# get morning report
+	today = ( datetime.datetime.utcnow() - datetime.timedelta(hours = 8) ).strftime("%Y/%m/%d")
+	if(request_key):
+		url2  = 'https://api.sikkasoft.com/v2/sikkanet_cards/Morning%20Report?request_key='+request_key+'&startdate='+today+'&enddate='+today
+		html2 = urlopen(url2)
+        	response = json.load(html2)
+		speech = "inside morning report loop"
+	
+	speech = speech + "Hello Sikka"
 
 	return {
 	 	"speech":speech,
