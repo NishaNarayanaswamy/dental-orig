2171523688108f7f7818a0e2d0a16141fd1852f0
NishaNarayanaswamy <nisha.narayanaswamy@sikkasoftware.com>
2017-02-16 01:23:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -25,20 +25,20 @@ def webhook():
 	return r
 
 def makeWebhookResult(req):
-	if req.get("result").get("action") != 'shipping.cost':
-		return {}
-	result = req.get("result")
-	parameters = result.get("parameters")
-	zone = parameters.get("shipping-zone")
+	#if req.get("result").get("action") != 'shipping.cost':
+	#	return {}
+	#result = req.get("result")
+	#parameters = result.get("parameters")
+	#zone = parameters.get("shipping-zone")
 
 	# define dictionary/database for cost
-	cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+	#cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
 	#speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-	speech = "hahaha blah"
+	speech = "blah connected"
 
-	print("Response:")
-	print(speech)
+	#print("Response:")
+	#print(speech)
 
 	return {
 	 	"speech":speech,
