078e8761da76ab6a955db8a3cd4797a48dc9e7de
NishaNarayanaswamy <nisha.narayanaswamy@sikkasoftware.com>
2017-02-24 15:07:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,24 @@ def makeWebhookResult(req):
 							speech = "You have "+str(count)+" appointments remaining for the day. Your next patient, " + patient_name +"... will arrive at "+first_apmnt+". Your last appointment is at " + last_apmnt.strftime("%I:%M %p")+"."
 				else:
 					speech = "You have no scheduled appointments today."
-
+					
+		elif req.get("result").get("action") == 'inventory':
+			speech = "Based on your scheduled appointments, you will require some additional supplies, totaling $100. ...Would you like to hear the list of supplies required?"
+		
+		elif req.get("result").get("action") == 'no_inventory':
+			speech = "Ok. Good-bye!"
+			
+		elif req.get("result").get("action") == 'read_first_item':
+			speech = "Kirkland Signature Nitrile Exam Glove Medium, 2 Pack, 200 Count, totaling $20. Would you like to order this item?"
+		
+		elif req.get("result").get("action") == 'confirm_item':
+			speech = "Item has been placed. Would you like to continue?"
+		
+		elif req.get("result").get("action") == 'read_next_item':
+			speech = "Walgreens Dental Mirror, Pick & Scaler Kit, 5 count, totaling $30. Would you like to order this item?"
+			
+		elif req.get("result").get("action") == 'invoice':
+			speech = "Item has been placed. An invoice of $50 has been emailed to you from Google Express."	
 
 	return {
 	 	"speech":speech,
